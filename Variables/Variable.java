a864215316f7a622485e824dee96acfc149badbd
sgrmo <myEmail@sagar.mohite11@yahoo.com>
2023-12-31 17:33:41 +0530

    variables added to remote on 31 dec 2023

---
+++ Variables/Variable.java
@@ -0,0 +1,17 @@
+package Variables;
+
+public class Variable {
+    public static void main(String[] args) {
+        // how to define the variable
+        // datatype variable_name = value;
+
+        byte b = 11;
+        short s = 12;
+        int i = 10;
+        long l = 154;
+        double d = 45.45;
+        float f = 47.45f;
+
+        System.out.println(f);
+    }
+}
